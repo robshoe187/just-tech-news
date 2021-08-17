2a942ae97467f0fbc1eb2bef621bcf1b53e5c8b6
Rob Shoemaker <rob.shoe187@gmail.com>
2021-08-17 10:53:55 -0400

    added upvote route

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
